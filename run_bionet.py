2f947c35f161905e42612811c5a83e68570e972b
Matthew Carroll <matthew.j.carroll@outlook.com>
2021-03-29 16:21:08 +0000

    adding output log

--- run_bionet.py
+++ run_bionet.py
@@ -116,7 +116,7 @@ def build_plots():
 
 if __name__ == '__main__':
     if __file__ != sys.argv[-1]:
-        run(sys.argv[-1])
+#        run(sys.argv[-1])
         build_plots()
     else:
         run('config.json')
